4be2287b84288f5224ea860d105311484d969193
Tangtang Zhou <tangtang2995@gmail.com>
2024-04-30 15:37:37 +0200

    chore: init repo with home-manager flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }: 
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."tangtang" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+      inherit home-manager;
+      inherit (home-manager) packages;
+    };
+}
---
+++ home.nix
@@ -0,0 +1,129 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "tangtang";
+  home.homeDirectory = "/home/tangtang";
+  home.packages = [
+    # new coreutils
+    pkgs.ripgrep
+    pkgs.fd
+    pkgs.bat
+    pkgs.bottom
+    pkgs.du-dust
+    pkgs.sd
+    pkgs.procs
+
+    # web tools
+    pkgs.wget
+    pkgs.httpie
+    pkgs.termscp
+
+    # file processors
+    pkgs.jq
+    pkgs.jqp
+    pkgs.yq
+    pkgs.zip
+    pkgs.unzip
+
+    # text editors
+    pkgs.vim
+    pkgs.delta
+
+    # languages
+    pkgs.rustup
+    (pkgs.python3.withPackages(ps: with ps; [pip]))
+    (pkgs.lua.withPackages(ps: with ps; [jsregexp]))
+    pkgs.gcc
+    pkgs.gnumake
+    pkgs.nodejs
+
+    # language tools
+    pkgs.tree-sitter
+
+    # language servers
+    pkgs.nil # nix
+    pkgs.marksman # markdown
+    pkgs.lua-language-server
+    pkgs.nodePackages.vscode-langservers-extracted # html, css, json, eslint
+    pkgs.nodePackages.typescript-language-server
+    pkgs.nodePackages.yaml-language-server
+    pkgs.taplo # toml
+
+    # formatters
+    pkgs.alejandra # nix
+    pkgs.nodePackages.prettier
+    pkgs.stylua
+    pkgs.shfmt # shell
+
+    # linters
+    pkgs.deadnix # nix
+    pkgs.shellcheck
+    pkgs.markdownlint-cli
+
+    # dap (debugger tool)
+    pkgs.vscode-extensions.vadimcn.vscode-lldb
+
+    # others
+    pkgs.fzf
+    pkgs.lazygit
+    pkgs.neofetch
+    pkgs.tealdeer
+    pkgs.wl-clipboard
+    pkgs.xdg-utils
+  ];
+  home.sessionVariables = { };
+
+  # zellij static config file (because limitation in nix to kdl converter)
+  xdg.configFile."zellij/config.kdl".source = ./config/zellij.kdl;
+  home.stateVersion = "23.11";
+  programs = {
+    fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set fish_greeting # Disable greeting
+      '';
+    };
+    eza = {
+      enable = true;
+      enableAliases = true;
+      icons = true;
+    };
+    zoxide = {
+      enable = true;
+    };
+    starship = {
+      enable = true;
+    };
+    zellij = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+    git = {
+      enable = true;
+      userName = "Tangtang Zhou";
+      userEmail = "tangtang2995@gmail.com";
+      aliases = {
+        pfl = "push --force-with-lease";
+        log1l = "log --oneline";
+      };
+      extraConfig = {
+        push = {
+          default = "current";
+          autoSetupRemote = true;
+        };
+        pull = {
+          rebase = true;
+        };
+        init = {
+          defaultBranch = "main";
+        };
+        credential.helper = "store";
+      };
+    };
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      vimAlias = true;
+    };
+  };
+}
