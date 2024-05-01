da7d8b0d2a04eb5fb36ce8f30dc1be3194f78071
Tangtang Zhou <tangtang2995@gmail.com>
2024-05-01 10:10:58 +0200

    chore(lazygit): add config

--- home.nix
+++ home.nix
@@ -63,7 +63,6 @@ in {
 
     # others
     pkgs.fzf
-    pkgs.lazygit
     pkgs.neofetch
     pkgs.tealdeer
     pkgs.wl-clipboard
@@ -132,6 +131,28 @@ in {
           defaultBranch = "main";
         };
         credential.helper = "store";
+      };
+    };
+    lazygit = {
+      enable = true;
+      settings = {
+        gui = {
+          theme = {
+            activeBorderColor = ["#89b4fa" "bold"];
+            inactiveBorderColor = ["#a6adc8"];
+            optionsTextColor = ["#89b4fa"];
+            selectedLineBgColor = ["#313244"];
+            cherryPickedCommitBgColor = ["#45475a"];
+            cherryPickedCommitFgColor = ["#89b4fa"];
+            unstagedChangesColor = ["#f38ba8"];
+            defaultFgColor = ["#cdd6f4"];
+            searchingActiveBorderColor = ["#f9e2af"];
+          };
+          authorColors = {
+            "Tangtang Zhou" = "#cba6f7";
+            "*" = "#b4befe";
+          };
+        };
       };
     };
     neovim = {
