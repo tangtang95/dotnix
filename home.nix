5daee884687f675977f58a03f53c327294b0ff27
Tangtang Zhou <tangtang2995@gmail.com>
2024-05-01 11:02:37 +0200

    chore(git): refactor into module

--- home.nix
+++ home.nix
@@ -131,37 +131,7 @@ in {
       enable = true;
       enableFishIntegration = true;
     };
-    git = {
-      enable = true;
-      userName = "Tangtang Zhou";
-      userEmail = "tangtang2995@gmail.com";
-      aliases = {
-        pfl = "push --force-with-lease";
-        log1l = "log --oneline";
-      };
-      delta = {
-        enable = true;
-      };
-      extraConfig = {
-        push = {
-          default = "current";
-          autoSetupRemote = true;
-        };
-        pull = {
-          rebase = true;
-        };
-        merge.tool = "nvimdiff";
-        mergetool = {
-          prompt = false;
-          keepBackup = false;
-        };
-        "mergetool \"vimdiff\"".layout = "LOCAL,MERGED,REMOTE";
-        init = {
-          defaultBranch = "main";
-        };
-        credential.helper = "store";
-      };
-    };
+    git = (import ./programs/git.nix);
     lazygit = {
       enable = true;
       settings = {
---
+++ programs/git.nix
@@ -0,0 +1,31 @@
+{
+  enable = true;
+  userName = "Tangtang Zhou";
+  userEmail = "tangtang2995@gmail.com";
+  aliases = {
+    pfl = "push --force-with-lease";
+    log1l = "log --oneline";
+  };
+  delta = {
+    enable = true;
+  };
+  extraConfig = {
+    push = {
+      default = "current";
+      autoSetupRemote = true;
+    };
+    pull = {
+      rebase = true;
+    };
+    merge.tool = "nvimdiff";
+    mergetool = {
+      prompt = false;
+      keepBackup = false;
+    };
+    "mergetool \"vimdiff\"".layout = "LOCAL,MERGED,REMOTE";
+    init = {
+      defaultBranch = "main";
+    };
+    credential.helper = "store";
+  };
+}
