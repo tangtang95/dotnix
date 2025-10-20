78c1a4465d6773e95e03d8aec77a603cb88ecb25
Tangtang Zhou <tangtang2995@gmail.com>
2025-10-20 22:13:29 +0200

    chore: revert xwayland enable

--- systems/tower/configuration.nix
+++ systems/tower/configuration.nix
@@ -96,7 +96,7 @@
       libnotify
       pkgs.unstable.rofi #TODO: after 25.05 remove unstable for 2.0.0 that supports wayland when available in stable
     ];
-    xwayland.enable = true;
+    xwayland.enable = false;
   };
   services.gnome.gnome-keyring.enable = true;
 
