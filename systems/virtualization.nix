c1aad6328dfb8eb5b4d0e9682cc2e19434edec45
Tangtang Zhou <tangtang2995@gmail.com>
2025-10-23 21:40:58 +0200

    chore: use qemu_kvm for virtualization

--- systems/virtualization.nix
+++ systems/virtualization.nix
@@ -24,6 +24,11 @@
     libvirtd = {
       enable = true;
       qemu = {
+        package = pkgs.qemu_kvm;
+        ovmf = {
+          enable = true;
+          packages = [pkgs.OVMFFull.fd];
+        };
         swtpm.enable = true;
       };
     };
