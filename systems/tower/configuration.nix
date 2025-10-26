6450eb2de29b213758d82faf29519896f2fd1e43
Tangtang Zhou <tangtang2995@gmail.com>
2025-10-26 14:25:07 +0100

    chore: enable auto nix store gc

--- systems/tower/configuration.nix
+++ systems/tower/configuration.nix
@@ -295,6 +295,13 @@
     };
   };
 
+  # Automatic garbage collection of nix store
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
